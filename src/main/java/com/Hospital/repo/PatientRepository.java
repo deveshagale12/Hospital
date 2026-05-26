f2f35ee328b9d52b9376413df9d6032ee73becc6
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-05-26 16:40:06 +0530

    email added

--- src/main/java/com/Hospital/repo/PatientRepository.java
+++ src/main/java/com/Hospital/repo/PatientRepository.java
@@ -11,4 +11,5 @@ import java.util.Optional;
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
     Optional<Patient> findByPatientNbr(Long patientNbr);
+    Optional<Patient> findByEmail(String email);
 }
