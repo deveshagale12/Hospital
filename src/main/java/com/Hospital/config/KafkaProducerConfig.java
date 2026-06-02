6831929936a9aca4c2e90ea841f99809e17f71f0
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-06-02 16:04:28 +0530

    kafka

---
+++ src/main/java/com/Hospital/config/KafkaProducerConfig.java
@@ -0,0 +1,48 @@
+
+package com.Hospital.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${KAFKA_USERNAME}")
+    private String username;
+
+    @Value("${KAFKA_PASSWORD}")
+    private String password;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        config.put("security.protocol", "SASL_SSL");
+        config.put("sasl.mechanism", "SCRAM-SHA-256");
+        config.put(
+            "sasl.jaas.config",
+            "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"" 
+            + username + "\" password=\"" + password + "\";"
+        );
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
